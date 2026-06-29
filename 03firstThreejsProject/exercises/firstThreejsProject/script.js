bcc6c4be62c61089bd8b936cf65c09d9a6de83e8
ElenaChanou <elenachanou87@gmail.com>
2026-06-29 19:18:17 +0300

    feat: create first 3D scene with a yellow cube

--- 03firstThreejsProject/exercises/firstThreejsProject/script.js
+++ 03firstThreejsProject/exercises/firstThreejsProject/script.js
@@ -19,4 +19,13 @@ const sizes = {
 
 //camera
 const camera = new  THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
 scene.add(camera)
+
+//renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
